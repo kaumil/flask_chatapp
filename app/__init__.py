34a38363093cd5395c520f1bfbd8e2246c5833cc
kaumil <kaumil.trivedi97@gmail.com>
2020-08-13 01:17:08 +0530

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask import Flask, request, current_app
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app)
+
+
+    if not app.debug:
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+    
+        file_handler = RotatingFileHandler('logs/flask_chatapp.log',maxBytes=10240,backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info("Flask Chatapp bootup")
+    
+    return app
